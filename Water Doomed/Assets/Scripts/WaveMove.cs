283beca7d3acc9169f3b5bef57ad5e7c8ad6c88e
Hamster <homakovich66@gmail.com>
2025-12-20 01:05:07 +0300

    Project

---
+++ Water Doomed/Assets/Scripts/WaveMove.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class WaveMove : MonoBehaviour
+{
+    public GameObject gameObject;
+    public void Awake()
+    {
+
+    }
+    void Start()
+    {
+        
+    }
+    void FixedUpdate()
+    {
+        
+    }
+}
